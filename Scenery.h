495f03ce2cf8587f6aed1c2f19f655112504a0a3
Igor <igorduarte.s@gmail.com>
2014-10-04 20:56:31 -0300

    Scenary.h

--- Scenery.h
+++ Scenery.h
@@ -6,24 +6,24 @@
 
 class Scenery {
     
-public:
-    Scenery();
-    void start();    
-private:
-    int *block_m;   
-    unsigned int variations;
-    
-    int algorithm(int *booth_c, int agent, int condition);
-    void init_block();
-    void set_block(int *booth);
-    void print_block();
-    bool is_null(int index);
-    int* malloc_block();
-    int* cp_pointer(int *p1);
-    void set_agent(const int agent, const int index);
-    int one_condition(int i);
-    int two_condition(int i);
-    int three_condition(int i);
+    public:
+        Scenery();
+        void start();    
+    private:
+        int *block_m;   
+        unsigned int variations;
+
+        int algorithm(int *booth_c, int agent, int condition);
+        void init_block();
+        void set_block(int *booth);
+        void print_block();
+        bool is_null(int index);
+        int* malloc_block();
+        int* cp_pointer(int *p1);
+        void set_agent(const int agent, const int index);
+        int one_condition(int i);
+        int two_condition(int i);
+        int three_condition(int i);
 
 };
 
